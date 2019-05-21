c7516d3cb30cca606cf6ae78e50f3b7dd2f990e8
sagexml <50482141+sagexml@users.noreply.github.com>
2019-05-21 11:40:08 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var input, score;
-function init(){
-    input= document.querySelector("#inputDisplay");
-    score= document.querySelector("#score");
-}
-
-function showScore(){
-    score.innerHTML=input.value;
-}
